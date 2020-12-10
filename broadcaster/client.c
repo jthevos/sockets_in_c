df4165f843cf414667bf8c3c847663aa6e75df15
John-Anthony Thevos <john.thevos@gmail.com>
2020-12-10 14:42:48 -0500

    Chat broadcaster

--- broadcaster/client.c
+++ broadcaster/client.c
@@ -1,144 +1,124 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<string.h>
-#include<stdlib.h>
-#include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 #define SERVER_PORT 5555
 #define MAX_LINE 256
 
-
-
 struct packet {
-	short seqNumber;
-	short type;
-	char uName[MAX_LINE];
-	char mName[MAX_LINE];
-	char data[MAX_LINE];
-	int groupNum;         // groupNum is included for multiple rooms in the future
+    short type;
+    char uName[MAX_LINE];
+    char mName[MAX_LINE];
+    char data[MAX_LINE];
+    short seqNumber;
+};
+
+struct registrationTable {
+    int port;
+    int sockid;
+    char mName[MAX_LINE];
+    char uName[MAX_LINE];
 };
 
 
-void *receive_thread(int *sock) {
-	int new_sock = *sock;
-	struct packet packet_chat;
-    packet_chat.type = htons(131);
-
-	while(1) {
-		if(recv(*sock, &packet_chat,sizeof(packet_chat),0) < 0) {
-			printf("\n Could not recieve chat packet. \n ");
-			exit(1);
-		}
-		else {
-			printf("\n%s: %s", packet_chat.uName, packet_chat.data);
-		}
-	}
-}
-
-
-int main(int argc, char* argv[]) {
-
-	struct packet packet_reg;
-	struct packet packet_conf;
-	struct packet packet_chat;
-
-	packet_reg.type = htons(121);
-	packet_conf.type = htons(221);
-	packet_chat.type = htons(131);
+int main(int argc, char *argv[]) {
 
 	struct hostent *hp;
 	struct sockaddr_in sin;
+	struct packet packet_reg;
+	struct packet packet_con;
+	struct packet packet_data_return;
+	char *user_name;
 	char *host;
 	char buf[MAX_LINE];
-	char clientname[MAX_LINE];
-	clientname[MAX_LINE];
 	int s;
 	int len;
-	int capacity = 0;
-	pthread_t threads[2];
 
-	gethostname(clientname, MAX_LINE);
-
-	if(argc == 4) {
+	if (argc == 3) {
 		host = argv[1];
-	}
-	else {
+		user_name = argv[2];
+	} else {
 		fprintf(stderr, "usage:newclient server\n");
 		exit(1);
 	}
 
-	hp = gethostbyname(host); //translate host name into peer's IP address
+	/* translate host name into peer's IP address */
+	hp = gethostbyname(host);
 
-	if(!hp) {
-			fprintf(stderr, "unkown host: %s\n", host);
-			exit(1);
+	if (!hp) {
+		fprintf(stderr, "unkown host: %s\n", host);
+		exit(1);
 	}
 
-	// keep an active open socket
-	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)  {
-			perror("tcpclient: socket");
-			exit(1);
+	/* active open */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("tcpclient: socket");
+		exit(1);
 	}
 
-	// build address data structure
-	bzero((char*)&sin, sizeof(sin));
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
 	sin.sin_family = AF_INET;
 	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
 	sin.sin_port = htons(SERVER_PORT);
 
-	//check to see if the client program has connected to the server.
-	if(connect(s,(struct sockaddr *)&sin, sizeof(sin)) < 0) {
+
+	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
 		perror("tcpclient: connect");
 		close(s);
 		exit(1);
 	}
-	else {
-		printf("%s\n", "Connection successfully established.");
-	}
 
-	// construct registration packet since successful connection
-	strcpy(packet_reg.mName, clientname);
-	strcpy(packet_reg.uName, argv[2]);
-	packet_reg.groupNum = atoi(argv[3]);
+	/*Create Registration Packet*/
+	packet_reg.type = htons(121);
 
-	// send three registration packets
-	for (int i = 0; i < 3; i++) {
+    // copy remaining host information to reg packet
+	char hostname[255];
+	int ret = gethostname(hostname, 255);
+	strcpy(packet_reg.mName, hostname);
+	strcpy(packet_reg.uName, user_name);
+
+    // send three registration packets
+	// start at 1 for clearer debug message printing in else block
+	for (int i = 1; i < 4; i++) {
 		if(send(s,&packet_reg,sizeof(packet_reg),0) < 0) {
 			printf("%s\n", "Registration packet failed to send. \n");
 			exit(1);
 		}
 		else {
-			printf("%s\n", "Registration packet has been sent successfully. \n");
+			printf("%s%d%s\n", "Registration packet ", i, " has been sent successfully. \n");
 		}
 	}
 
-	//once the packets have been sent, we wait and receive the acknowledgment from the server
-	if(recv(s,&packet_conf,sizeof(packet_conf),0) < 0) {
+
+	/*Receives the Confirmation Code*/
+	if (recv(s, &packet_con, sizeof(packet_con), 0) < 0) {
 		printf("%s\n", "Confirmation packet failed to be received. \n");
 		exit(1);
 	}
-	else {
-		printf("%s\n", "Confirmation packet has been received successfully. \n");
+
+    // type check confirm code
+	if (ntohs(packet_con.type) != 221) {
+		printf("%s\n", "Incorrect confirmation code\n");
+		printf("%d\n", ntohs(packet_con.type));
+		exit(1);
 	}
 
-	// since the ack was successful, we create the thread
-	pthread_create(&threads[0],NULL,(void *)receive_thread,&s);
-
-	// be constantly listening for stdin input, thus while True
-	while(1) {
-
-		fgets(buf, sizeof(buf), stdin);
-		buf[MAX_LINE-1] = '\0';
-		len = strlen(buf) + 1;
-
-		strcpy(packet_chat.data, buf);
-		strcpy(packet_chat.uName, argv[2]);
-
-		packet_chat.groupNum = atoi(argv[3]);
-		send(s, &packet_chat, sizeof(packet_chat), 0);
-
+    // print data being sent from server
+	while (recv(s, &packet_data_return, sizeof(packet_data_return), 0) > 0) {
+		if (ntohs(packet_data_return.type) != 131) {
+			printf("Wrong Data Packet Type\n");
+			exit(1);
+		} else {
+            printf("Received Data Seq: %d\n", packet_data_return.seqNumber);
+    		printf("%s\n", packet_data_return.data);
+        }
 	}
+
 }
--- broadcaster/server.c
+++ broadcaster/server.c
@@ -1,238 +1,216 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<string.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<fcntl.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #define SERVER_PORT 5555
 #define MAX_LINE 256
 #define MAX_PENDING 5
 
-int clients = 1; // clients represents the number of clients connected
-int bufferindex = 0;
-int arrayIndex = 0;
-pthread_mutex_t table_mutex = PTHREAD_MUTEX_INITIALIZER;		// global mutex variable
-pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
-struct packet //packet structure for all the packets we are sending and receiving
-{
-	short seqNumber;
-	short type;
-	char uName[MAX_LINE];
-	char mName[MAX_LINE];
-	char data[MAX_LINE];
-	int groupNum;
+struct packet {
+    short type;
+    char uName[MAX_LINE];
+    char mName[MAX_LINE];
+    char data[MAX_LINE];
+    short seqNumber;
 };
-struct registrationTable //table structure to store all the information of the clients who are connecting to the server
-{
-	short seqNumber;
-	int port;
-	int sockid;
-	char mName[MAX_LINE];
-	char uName[MAX_LINE];
-	int groupNum;
-};
-struct packet buffer[256];
-struct registrationTable client_info[10];
 
-//join handler thread that receives registration packets and inputs the client's data into the table
-void *join_handler(struct registrationTable *clientData)
-{
-	//field of variables that are used in the join handler thread
-	int newsock;
-	newsock = clientData->sockid;
+struct registrationTable {
+    int port;
+    int sockid;
+    char mName[MAX_LINE];
+    char uName[MAX_LINE];
+};
+
+
+pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+struct registrationTable table[10];
+int table_index = 0;
+pthread_t threads[2];
+
+/*Method Initialization*/
+void *join_handler(struct registrationTable *clientData);
+void *chat_multicaster();
+
+int main(int argc, char *argv[]) {
 	struct sockaddr_in sin;
 	struct sockaddr_in clientAddr;
-	bzero((char*)&sin, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_port = htons(SERVER_PORT);
 	struct packet packet_reg;
-	packet_reg.type = htons(121);
-	struct packet packet_conf;
-	packet_conf.type = htons(221);
-	struct packet packet_chat;
-	packet_chat.type = htons(131);
+	struct packet packet_con;
+	struct packet packet_data;
+	struct packet packet_data_return;
+	struct registrationTable client_info;
+	char buf[MAX_LINE];
+	int s, new_s, len, exit_value;
 
-	if(recv(newsock,&packet_reg,sizeof(packet_reg),0)<0) //receiving the second registration packet from the client
-	{
-		printf("\n Could not receive\n");
-		exit(1);
-	}
-	else
-	{
-		printf("\n Client has been registered\n"); //receiving the third registration packet from the client
-	}
-	if(recv(newsock,&packet_reg,sizeof(packet_reg),0)<0)
-	{
-		printf("\n Could not receive\n");
-		exit(1);
-	}
-	else
-	{
-		printf("\n Client has been registered\n");
-	}
-	if(send(newsock,&packet_conf,sizeof(packet_conf),0)<0)
-	{
-		printf("fail");
-		exit(1);
-	}
-	else
-	{
-	printf("\n Confirmation packet has been sent\n");
-	}
-
-	pthread_mutex_lock(&table_mutex);
-	//add the client's info into the table
-	client_info[clients].port = clientAddr.sin_port;
-	client_info[clients].sockid = newsock;
-	strcpy(client_info[clients].uName, packet_reg.uName);
-	strcpy(client_info[clients].mName, packet_reg.mName);
-	client_info[clients].groupNum = packet_reg.groupNum;
-	clients++;
-	pthread_mutex_unlock(&table_mutex);
-	//when the two registration packets are received from the client, then the join handler will send
-	//an acknowledgement packet
-
-	while(1)
-	{
-		if(recv(newsock, &packet_chat,sizeof(packet_chat),0) < 0)
-		{
-			printf("No");
-			exit(1);
-		}
-		else
-		{
-		pthread_mutex_lock(&buffer_mutex);
-		buffer[arrayIndex].groupNum = packet_chat.groupNum;
-		strcpy(buffer[arrayIndex].data, packet_chat.data);
-		strcpy(buffer[arrayIndex].uName, packet_chat.uName);
-		arrayIndex++;
-		pthread_mutex_unlock(&buffer_mutex);
-		}
-	}
-}
-
-//chat multicaster that will send data constantly while there is atleast one client connected.
-void *chat_multicaster()
-{
-	//different variables that are used in the chat multicaster
-	struct packet temp;
-	int groupNum;
-	int i;
-	int s;
-	int nread;
-	struct packet packet_chat;
-	packet_chat.type = htons(131);
-	//infinity loop so that the text file will not stop sending
-	while(1)
-	{
-	//this while loop will only send if there is atleast one client connect, and will constantly
-	//check to see when a client had finally connected
-		if(clients > 1)
-		{
-
-			//store the packet at an index into a temp packet
-			if(bufferindex < arrayIndex)
-			{
-				pthread_mutex_lock(&buffer_mutex);
-
-				temp.groupNum = buffer[bufferindex].groupNum;
-				strcpy(temp.data, buffer[bufferindex].data);
-				strcpy(temp.uName, buffer[bufferindex].uName);
-
-				pthread_mutex_unlock(&buffer_mutex);
-				pthread_mutex_lock(&table_mutex);
-
-				for ( i = 1; i < clients; i++) //a for loop that will go through the client info table and send the chat data to everone who is in the table
-				{
-					s = client_info[i].sockid;
-					if(client_info[i].groupNum == temp.groupNum)
-					{
-						if(send(s, &temp,sizeof(temp),0)<0)
-						{
-							printf("Chat packet was not received \n");
-							exit(1);
-						}
-						else
-						{
-							printf("sent to: %s ", client_info[i].uName);
-						}
-					}
-				}
-				printf("\n%s: %s ", temp.uName, temp.data);
-				bufferindex++;
-				//unlock the table so join handler can access
-				pthread_mutex_unlock(&table_mutex);
-			}
-		}
-	}
-}
-
-int main(int argc, char* argv[])
-{
-	//declare variables and initialize the types of the packets used
-	struct packet packet_reg;
-	packet_reg.type = htons(121);
-	void *exit_value;
-	struct sockaddr_in sin;
-	struct sockaddr_in clientAddr;
-	pthread_t threads[2]; 										//we will need two threads on the server
-	int s, new_s;
-	int len;
-	int cindex = 0;
-
-	//create the multicaster
-	pthread_create(&threads[1],NULL,chat_multicaster,NULL);
-
+	int error = pthread_create(&threads[1], NULL, chat_multicaster, NULL);
 
 	/* setup passive open */
-	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-	{
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("tcpserver: socket");
 		exit(1);
 	}
+
 	/* build address data structure */
-	bzero((char*)&sin, sizeof(sin));
+	bzero((char *)&sin, sizeof(sin));
 	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = INADDR_ANY;
 	sin.sin_port = htons(SERVER_PORT);
 
-	if(bind(s,(struct sockaddr *)&sin, sizeof(sin)) < 0)
-	{
+	if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
 		perror("tcpclient: bind");
 		exit(1);
 	}
 	listen(s, MAX_PENDING);
 
-			//checks to see if the client is accepted
-	while(1)
-	{
-	   if((new_s = accept(s, (struct sockaddr *)&clientAddr, &len)) < 0)
-		{
+
+	while (1) {
+		if ((new_s = accept(s, (struct sockaddr *)&clientAddr, &len)) < 0) {
 			perror("tcpserver: accept");
 			exit(1);
-		}
-		else
-		{
+		} else {
 			printf("\n Client has been connected\n");
+            printf("\nClient's port is %d \n", ntohs(clientAddr.sin_port));
+		}
 
+
+		//1st Registration packet receive and Create Join Handler
+		if (recv(new_s, &packet_reg, sizeof(packet_reg), 0) < 0) {
+			printf("\n Could not receive first registration packet\n");
+			exit(1);
 		}
-		//accept the first registration packet so we know to make the join handler
-		if(recv(new_s,&packet_reg,sizeof(packet_reg),0)<0)
-		{
-			printf("Registration packet failed to be received");
+
+		if (ntohs(packet_reg.type) != 121) {
+			printf("\nNot the registration code\n");
+			exit(1);
 		}
-		else
-		{
-			printf("\n Client has been registered\n");
-			client_info[0].sockid = new_s;
-		}
-		//creating of the join handler thread in the server
-		pthread_create(&threads[0],NULL,(void *)join_handler,&client_info);
+
+		//Puts Registered Machine into Client_info
+		pthread_mutex_lock(&my_mutex);
+
+		client_info.port = clientAddr.sin_port;
+		client_info.sockid = new_s;
+		strcpy(client_info.uName, packet_reg.uName);
+		strcpy(client_info.mName, packet_reg.mName);
+		pthread_mutex_unlock(&my_mutex);
+
+		printf("* Message Type: %d\n", ntohs(packet_reg.type));
+		printf("* User Name: %s\n", client_info.uName);
+		printf("* Machine Name: %s\n", client_info.mName);
+
+		//Server Creates Join Handler
+		exit_value = pthread_create(&threads[0], NULL, join_handler, &client_info);
+		int err = pthread_join(threads[0], &exit_value);
 	}
+}
+
+/*Join Handler*/
+void *join_handler(struct registrationTable *clientData)
+{
+	struct packet packet_reg;
+	struct packet packet_con;
+	struct registrationTable client_info;
+	int newsock = clientData->sockid;
+
+
+	// handle second registration packet
+	if (recv(newsock, &packet_reg, sizeof(packet_reg), 0) < 0) {
+		printf("\nCould not receive 2nd registration packet\n");
+		exit(1);
+	} else {
+        if (ntohs(packet_reg.type) != 121) {
+            printf("%s%d\n", "Incorrect registration code. Recieved: ", ntohs(packet_reg.type));
+            exit(1);
+        } else {
+            printf("Received second registration packet\n");
+        }
+    }
+
+
+	// handle third registration packet
+	if (recv(newsock, &packet_reg, sizeof(packet_reg), 0) < 0) {
+		printf("\nCould not receive 3rd registration packet\n");
+		exit(1);
+	} else {
+        if (ntohs(packet_reg.type) != 121) {
+            printf("%s%d\n", "Incorrect registration code. Recieved: ", ntohs(packet_reg.type));
+            exit(1);
+        } else {
+            printf("Received third registration packet\n");
+        }
+    }
+
+
+	// Update the table
+	pthread_mutex_lock(&my_mutex);
+
+	//Puts Registered Machine into Table and Increments Index
+	table[table_index].port = clientData->port;
+	table[table_index].sockid = newsock;
+	strcpy(table[table_index].uName, clientData->uName);
+	strcpy(table[table_index].mName, clientData->mName);
+	table_index++;
+
+	pthread_mutex_unlock(&my_mutex);
+
+	// Send acknowledgement/confirmation to the client
+	packet_con.type = htons(221);
+
+	if (send(newsock, &packet_con, sizeof(packet_con), 0) < 0) {
+		printf("\n Send Confirmation Failed\n");
+		exit(1);
+	} else {
+        printf("Sent Confirmtion Packet\n");
+    }
+}
+
+void *chat_multicaster()
+{
+	char *filename;
+	char text[100];
+	ssize_t nread;
+	struct packet packet_data;
+	int fd, index;
+	int seq_num = 1;
+
+	filename = "input.txt";
+	fd = open(filename, O_RDONLY, 0);
+
+	// We want the server to be constantly sending, thus while 1
+	while (1) {
+        // If at least one client is listed...
+		while (table_index > 0) {
+            // ...read 100 bytes of data from input
+			while (read(fd, text, 100)) {
+				// Construct the data packet
+				packet_data.type = htons(131);
+				packet_data.seqNumber = seq_num;
+				strcpy(packet_data.data, text);
+
+				// Send data packets to each client listed on the table
+				for (int i = 0; i < 10; i++) {
+					int newsock = table[i].sockid;
+					if (newsock != 0) {
+						if (send(newsock, &packet_data, sizeof(packet_data), 0) < 0)
+						{
+							printf("\nFailed to send multicaster data.\n");
+							exit(1);
+						}
+					}
+					pthread_mutex_unlock(&my_mutex);
+				}
+				seq_num++;
+				sleep(2);
+
+				printf("Sent to clients\n");
+			}
+		}
+	}
+
 }
